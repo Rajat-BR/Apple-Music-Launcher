ea002d5de3acea6dd2990ec87df0b002cc033a10
Rajat <rajatbr13@gmail.com>
2026-07-27 18:27:36 +0530

    feat: validate launcher configuration

--- launcher.py
+++ launcher.py
@@ -7,8 +7,18 @@ config_path = Path(__file__).parent / "config.json"
 with open(config_path, 'r') as f:
     config = json.load(f)
     
-if config["gui_path"] == "":
+if not config["gui_path"]:
         print("GUI path is not configured !")
         sys.exit()
 
+gui_path = Path(config["gui_path"])
 
+#Check if the path doesn't exist
+if not gui_path.exists():
+    print("Path doesn't exist !")
+    sys.exit()
+
+#Check if the path is not a directory
+if not gui_path.is_dir():
+    print("GUI path must be a directory")
+    sys.exit()
