bb2051ec3cb83e859a59de0803c299637b0e10ef
Rajat <rajatbr13@gmail.com>
2026-07-27 22:34:32 +0530

    feat(wrapper): start wrapper using configured path

--- launcher.py
+++ launcher.py
@@ -54,6 +54,7 @@ def wait_for_docker(timeout=60):
 
         time.sleep(1)
 
+
 config_path = Path(__file__).parent / "config.json"
 
 with open(config_path, 'r') as f:
@@ -75,5 +76,48 @@ if not gui_path.is_dir():
     print("GUI path must be a directory")
     sys.exit()
 
+# Check if the path is empty
+if not config["wrapper_path"]:
+    print("Wrapper path is not configured.")
+    sys.exit()
+
+wrapper_path = Path(config["wrapper_path"])
+
+# Check if the path doesn't exist
+if not wrapper_path.exists():
+    print("Wrapper path doesn't exist.")
+    sys.exit()
+
+# Check if the path is not a directory
+if not wrapper_path.is_dir():
+    print("Wrapper path must be a directory.")
+    sys.exit()
+
+
+def start_wrapper():
+
+    print("Starting wrapper container...")
+    wrapper_data = wrapper_path / "rootfs" / "data"
+    wrapper_data.mkdir(parents=True, exist_ok=True)
+
+    try:
+        subprocess.Popen(
+            [
+                "docker",
+                "run",
+                "-v", f"{wrapper_data}:/app/rootfs/data",
+                "-p", "10020:10020",
+                "-p", "20020:20020",
+                "-e", "args=-M 20020 -H 0.0.0.0",
+                "--rm",
+                "ghcr.io/itouakirai/wrapper:x86"
+            ]
+        )
+
+    except FileNotFoundError:
+        print("Docker CLI not found.")
+        sys.exit(1)
+
 start_docker_desktop()
 wait_for_docker()
+start_wrapper()
