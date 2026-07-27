ca3582288d9629c8e6084a168658a8479a4ad0da
Rajat <rajatbr13@gmail.com>
2026-07-27 18:18:39 +0530

    feat: validate GUI path and exit on empty path

--- launcher.py
+++ launcher.py
@@ -1,3 +1,4 @@
+import sys
 import json
 from pathlib import Path
 
@@ -5,4 +6,9 @@ config_path = Path(__file__).parent / "config.json"
 
 with open(config_path, 'r') as f:
     config = json.load(f)
+    
+if config["gui_path"] == "":
+        print("GUI path is not configured !")
+        sys.exit()
+
 
